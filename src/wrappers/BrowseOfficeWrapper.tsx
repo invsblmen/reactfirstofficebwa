d77e14c6440654617487ed2ddc38531b468b2a4c
invsblmen <43681019+invsblmen@users.noreply.github.com>
2024-11-06 12:55:44 +0700

    api data office

--- src/wrappers/BrowseOfficeWrapper.tsx
+++ src/wrappers/BrowseOfficeWrapper.tsx
@@ -1,6 +1,41 @@
+import { useEffect, useState } from "react"
 import OfficeCard from "../components/OfficeCard"
+import axios from "axios";
+import { Office } from "../types/type";
 
 export default function BrowseOfficeWrapper() {
+
+  const [offices, setOffices] = useState<Office[]>([]);
+
+  const [loading, setLoading] = useState(true);
+  
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    axios
+      .get("http://laravel11_firstofficebebwa.test/api/offices", {
+        headers: {
+          "X-API-KEY": "dsajdkadjak12132131",
+        },
+      })
+      .then((response) => {
+        setOffices(response.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      })
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error loading data: {error}</div>;
+  }
+
   return (
     <section
         id="Fresh-Space"
@@ -12,7 +47,9 @@ export default function BrowseOfficeWrapper() {
           For Your Better Productivity.
         </h2>
         <div className="grid grid-cols-3 gap-[30px]">
-          <OfficeCard></OfficeCard>
+          {offices.map((office) => (
+            <OfficeCard key={office.id} office={office}></OfficeCard>
+          ))}
         </div>
       </section>
   )
